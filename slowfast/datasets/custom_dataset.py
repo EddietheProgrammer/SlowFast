70f63ee7f35c41cc01d6e626d8fc130f1d028ddd
EddietheProgrammer <124279660+EddietheProgrammer@users.noreply.github.com>
2025-07-28 14:32:26 -0400

    patching decoder.py

--- slowfast/datasets/custom_dataset.py
+++
@@ -1,8 +0,0 @@
-from slowfast.datasets.build import DATASET_REGISTRY
-from slowfast.datasets.kinetics import Kinetics
-
-@DATASET_REGISTRY.register()
-class Custom(Kinetics):
-    def __init__(self, cfg, split):
-        super().__init__(cfg, split)
-        # You can override methods here if needed
